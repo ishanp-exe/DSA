b72ffef4cbedd9e6f7fb19f5f18924e538308b85
ishanp-exe <ishanpandey1610@gmail.com>
2026-06-20 17:27:51 +0530

    LeetCode #410(H)

---
+++ Split_Array.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/split-array-largest-sum/
+import java.util.*;
+
+public class Split_Array {
+    static int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+        for(int i=0; i<nums.length; i++){
+            start = Math.max(start, nums[i]); //int the end of the loop this will contain the max item from the array
+            end += nums[i];
+        }
+
+        //binary search
+        while(start < end){
+            // try for the middle as pootential answer
+            int mid = start + (end-start)/2;
+            
+            //calculate how many pieces u can divide this in with max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums){
+                if(sum + num > mid){
+                    //you cannot add this in this subarray, make new one
+                    //say u add this num in the new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                }else{
+                    sum += num;
+                }
+            }
+            if(pieces > k){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return end; //here start == end
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0; i<nums.length; i++){
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(splitArray(nums,k));
+    }
+}
