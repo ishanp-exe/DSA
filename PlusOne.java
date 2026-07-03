3c3e2f84c5ace7033bef011b509ed6442e49282a
ishanp-exe <ishanpandey1610@gmail.com>
2026-07-03 23:10:53 +0530

    LeetCode #66(E)

---
+++ PlusOne.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/plus-one/description/
+
+import java.util.*;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        PlusOne obj = new PlusOne();
+        System.out.println(Arrays.toString(obj.plusOne(arr)));
+    }
+}
