35ef18864f472a83d9b48c73acfd7983027745b6
ishanp-exe <ishanpandey1610@gmail.com>
2026-07-11 23:31:50 +0530

    LeetCode #55(M)

---
+++ Jump_Game.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/jump-game/description/
+
+import java.util.*;
+public class Jump_Game {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Jump_Game obj = new Jump_Game();
+       System.out.println(obj.canJump(arr));
+    }
+}
